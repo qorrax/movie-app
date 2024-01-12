432ab6a142fe18eecc7a742f23f15e406b9ba21c
qorrax <simplecarrier101@gmail.com>
2024-01-12 00:45:17 -0500

    addes some code for the api call and the movies api

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+
+
+
+import React from 'react';
 
 const MovieCard = (props) => {
+  
+  // Check if film is present
+  if (!props.film) {
+    return <div>No movie data available</div>;
+  }
 
-    console.log("props", props)
+  // Destructure nested objects
+  const {
+    description,
+    director,
+    title,
+    release_date,
+    image,
+    movie_banner,
+    original_title,
+    original_title_romanised,
+    producer,
+    rt_score,
+    running_time,
+    url,
+  } = props.film;
+
   return (
     <div>
-      <h1> this is the card for one movie</h1>
+      <h1>{title}</h1>
+      <h3>{director}</h3>
+      <p>{description}</p>
+      <p>Release Date: {release_date}</p>
+      {/* Add more information as needed */}
+      <img src={image} alt={title} />
+      {/* You can use other data fields as needed */}
     </div>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
+
+
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import MovieCard from './MovieCard'
 
@@ -6,17 +6,18 @@ const  MovieList= () => {
 
     const [movies, setMovies] = useState([])
 
+    
      
 
     useEffect(() => {
 
-        axios.get('https://api.themoviedb.org/3/movie/550?api_key=14d3b4a12d28d5a2b303a64e680e6cda')
-        .then(response => {
+        axios.get("https://ghibliapi.vercel.app/films/")
+        .then((response) => {
             setMovies(response.data)
 
         })
         .catch(error => {
-            setErrorMessage(error.message)
+            console.log(error)
         })
 
     }, [])
@@ -25,11 +26,11 @@ const  MovieList= () => {
   return (
     <div>
       <h1> here are movie list </h1>
-      { movies.map(function(movie)  {
+       {movies.map(function(movie)  {
            return <MovieCard film={movie} />   
            
-        })}
-     </div>
+        })}  
+     </div> 
   )
 }
 
